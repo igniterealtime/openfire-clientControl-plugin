77272eac102d1643394fef4c4fb4a40952215922
daryl herzmann <akrherz@iastate.edu>
2017-01-04 01:35:39 -0600

    remove legacy CVS keywords (#734)

--- src/java/org/jivesoftware/openfire/plugin/spark/manager/SparkVersionManager.java
+++ src/java/org/jivesoftware/openfire/plugin/spark/manager/SparkVersionManager.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (C) 1999-2008 Jive Software. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,8 +45,8 @@ import org.xmpp.packet.PacketError;
  */
 public class SparkVersionManager implements Component {
     
-    private static final Logger Log = LoggerFactory.getLogger(SparkVersionManager.class);
-    
+	private static final Logger Log = LoggerFactory.getLogger(SparkVersionManager.class);
+	
     private ComponentManager componentManager;
     public static String SERVICE_NAME = "updater";
 
